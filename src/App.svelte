9a05ae00ba91ac3f75f6cc21d953a07db0a8eeb3
afpsujek <afpsujek@gmail.com>
2021-04-04 13:40:38 -0500

    handle chip selection

--- src/App.svelte
+++ src/App.svelte
@@ -4,22 +4,32 @@
 	import { onMount } from 'svelte';
 	import { genres } from './store/genre'
 	import { session } from './store/session';
+	import type { Genre } from './models/genre';
 
 	onMount(async () => {
 		const sessionData = await axios.get(`https://api.themoviedb.org/3/authentication/guest_session/new?api_key=${globalThis.__TMD_API_KEY__}`);
 		const genreData = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${globalThis.__TMD_API_KEY__}&language=en-US`)
 		session.update(() => sessionData.data.guest_session_id);
-		genres.update(() => genreData.data.genres);
+		genres.update(() => genreData.data.genres.map(genre => {
+				return {...genre, selected: false};
+			})
+		)
 	});
+
+	function onSelected(selected: Genre) {
+		genres.update(genres => genres.map(genre => {
+			return selected.id === genre.id ? {...genre, selected: !genre.selected} : genre
+		}))
+	}
 </script>
 
 <main>
 	<h1>Don't know what movie to watch?</h1>
 	<h2>Let me help with that</h2>
-	
+
 	{#if $genres}
 		{#each $genres as genre}
-		<Chip></Chip>
+			<Chip on:click={() => onSelected(genre)} genre={genre}></Chip>
 		{/each}
 	{/if}
 </main>
--- src/components/Chip.svelte
+++ src/components/Chip.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
-
+    import type { Genre } from "../models/genre";
+    export let genre: Genre;
 </script>
 
 <style>
     .chip {
-        width: 4em;
+        width: 10em;
         border: solid 1px black;
         border-radius: 10px;
         text-align: center;
+        margin: 5px;
+        cursor: pointer;
+    }
+
+    .selected {
+        background-color: lightgray;
     }
 </style>
 
-<div class="chip">
-    Chip
+<div on:click
+    class="{genre.selected ? 'selected' : ''} chip">
+    {genre.name}
+    {genre.selected}
 </div>
