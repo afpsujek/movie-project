14aacce94cbab9a603e0972e2a7f7f2112abd45c
afpsujek <afpsujek@gmail.com>
2021-04-11 16:57:02 -0500

    derived genre store

--- src/components/Suggestion.svelte
+++ src/components/Suggestion.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { selectedGenres } from '../store/genre';
+    import { session } from '../store/session';
 </script>
 
 <style>
@@ -11,5 +13,9 @@
 </style>
 
 <div class="suggestion">
-    <h2>Suggestion works!</h2>
+    {#if $selectedGenres?.length === 0}
+        <h3>Select some genres to get started</h3>
+    {/if}
+    
+    <button on:click="">Get a movie selection</button>
 </div>
--- src/store/genre.ts
+++ src/store/genre.ts
@@ -1,4 +1,6 @@
-import { writable } from 'svelte/store';
+import { derived, writable } from 'svelte/store';
 import type { Genre } from '../models/genre';
 
 export const genres = writable<Genre[]>(undefined);
+
+export const selectedGenres = derived(genres, $genres => $genres?.filter(genre => genre.selected));
